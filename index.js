a5d2f70a53cc6de9dd68b7135acdb193c0ce0afa
Danny Hobart <dh@dannyhobart.com>
2015-04-24 13:40:15 -0400

    fleshing out the basics

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+,fs = require('fs')
+,strava = require('strava-v3');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + "/app"));
+
+app.listen(app.get('port'), function(){
+	console.log('Node app is running at localhost:' + app.get('port'));
+})
